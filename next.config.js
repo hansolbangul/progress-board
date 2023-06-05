28729ace1c2227938a43fb9cc9a40944a3facc92
hansolbangul <71314689+hansolbangul@users.noreply.github.com>
2023-06-06 00:16:23 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: false,
+  images: {
+    domains: ["links.papareact.com", "cloud.appwrite.io"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
